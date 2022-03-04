6db263ed26451b4fc462bee1702ca99bc5e63408
wuchuang <wuchuang456@126.com>
2022-03-04 20:33:30 +0800

    添加特效球

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 const home = () => import('@views/index/home/home');
+const test = () => import('@views/test');
 Vue.use(VueRouter);
 const routes = [
   { path: '/', name: 'home', component: home },
+  { path: '/test', name: 'test', component: test },
 ];
 const router = new VueRouter({ routes, mode: 'history' });
 export default router;
